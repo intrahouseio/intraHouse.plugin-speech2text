b85b8f2f876d3a0963f8478f29ac40a22979c050
Artem Ivanov <fast0490f@mail.ru>
2019-02-12 16:13:05 +0300

    Dtest

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
-sudo apt-get install sox libsox-fmt-all
-brew install sox
+sudo apt-get install python-pyaudio python3-pyaudio sox libsox-fmt-all
+sudo apt-get install libatlas-base-dev
+brew install swig portaudio sox
 exit 0
